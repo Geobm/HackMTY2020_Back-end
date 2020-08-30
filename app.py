d2dda78445ea4973a4ec9ce7ddbcae02592d00f0
Genaro Almaraz <jg08950@gmail.com>
2020-08-29 21:26:16 -0500

    Get user programming languages

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 from flask import Flask, jsonify
 from github import github_token
+
 import requests
 import os
 from pprint import pprint
@@ -8,7 +9,28 @@ app = Flask(__name__)
 
 @app.route('/users/<usernames>')
 def analyze_match(usernames):
-    return jsonify(data=usernames)
+    token = os.getenv('GITHUB_TOKEN', github_token)
+
+    names = usernames.split(',')
+
+    match = []
+    for name in names:
+        query_url = f"https://api.github.com/users/{name}/repos"
+        params = {
+            "state": "open",
+        }
+        headers = {'Authorization': f'token {token}'}
+        response = requests.get(query_url, headers=headers, params=params)
+        result = response.json()
+    
+        languages = []
+        for i in range(len(result)):
+            if result[i]["language"] is not None:
+                languages.append(result[i]["language"])
+
+        match.append(languages)
+ 
+    return jsonify(data=match)
 
 if __name__ == '__main__':
     app.run()
