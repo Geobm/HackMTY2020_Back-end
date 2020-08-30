6bc99538b334a4e593a29e6f999c294fdc61200d
Genaro Almaraz <jg08950@gmail.com>
2020-08-29 20:21:55 -0500

    Get usernames param

--- app.py
+++ app.py
@@ -1,10 +1,36 @@
 from flask import Flask, jsonify
 
+from token import github_token
+
+import requests
+import os
+from pprint import pprint
+
 app = Flask(__name__)
 
-@app.route('/')
-def say_hello():
-    return jsonify(data="Hello World")
+@app.route('/users/<usernames>')
+def analyze_match(usernames):
+    token = os.getenv('GITHUB_TOKEN', github_token)
+
+    result = []
+
+    for username in usernames:
+        query_url = f"https://api.github.com/users/{username}/repos"
+        params = {
+            "state": "open",
+        }
+        headers = {'Authorization': f'token {token}'}
+        r = requests.get(query_url, headers=headers, params=params)
+        x = (r.json())
+
+        y = []
+        for i in range(len(x)):
+            if x[i]["language"] is not None:
+                y.append(x[i]["language"])
+        
+        result.append(y)
+
+    return jsonify(data=result)
 
 
 if __name__ == '__main__':
