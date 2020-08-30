1a4d262ae35757937d8af7afba35c3736b399729
Genaro Almaraz <jg08950@gmail.com>
2020-08-30 00:31:33 -0500

    Match per person and user info

--- app.py
+++ app.py
@@ -43,8 +43,51 @@ def analyze_match(usernames):
         i += 1
 
     average = sum(ratios) / len(ratios)
+
+    # Combinations
+    i = 0
+    info = []
+    for i in range(len(match)):
+        if i < len(match):
+            j = i + 1
+            
+            ratios = {}
+            user_data = {}
+
+            for j in range(len(match)):
+                if names[i] != names[j]:
+                    aux = difflib.SequenceMatcher(None, match[i], match[j])
+                    ratios[names[j]] = aux.ratio()
+
+                j += 1
+
+            maximum = max(ratios.values())
+
+            keys = list(ratios.keys())
+            values = list(ratios.values())
+
+            person = keys[values.index(maximum)]
+
+            user_data['user_name'] = names[i]
+            user_data['person'] = person
+            user_data['ratio'] = maximum
+
+            query_url = f"https://api.github.com/users/{names[i]}/repos"
+            params = {
+                "state": "open",
+            }
+            headers = {'Authorization': f'token {token}'}
+            response = requests.get(query_url, headers=headers, params=params)
+            result = response.json()
+
+            if result:
+                user_data['avatar'] = result[0]['owner']['avatar_url']
+            else:
+                user_data['avatar'] = None
+
+            info.append(user_data)
     
-    return jsonify(total="{:.2f}".format(average * 100))
+    return jsonify(total="{:.2f}".format(average * 100), combinations=info)
 
 if __name__ == '__main__':
     app.run()
